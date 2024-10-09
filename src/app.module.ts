f2bc19cf273fa47f7df3a1c91b3f2abd7b43b1d9
Bini <binilulseged@gmail.com>
2024-10-09 11:54:31 +0300

    updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,17 +6,19 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+
   imports: [
     UsersModule,
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({  envFilePath: '.development.env',
+    }),
     TypeOrmModule.forRoot({
-      type: process.env.DB_TYPE as any,
+      type: process.env.DB_TYPE as 'postgres',
       host: process.env.PG_HOST,
-      port: parseInt(process.env.PG_PORT),
+      port: parseInt(process.env.PG_PORT, 10)  || 5432,
       username: process.env.PG_USER,
       password: process.env.PG_PASSWORD,
       database:process.env.PG_DB,
-      entities: [__dirname + '**/*.entity{.ts,.js}'],
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     })
   ],
